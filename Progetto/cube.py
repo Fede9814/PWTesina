f74adaee9d4e0ee4b9e12a7531dfa2d7fc6c5b60
Fede9814 <47415763+Fede9814@users.noreply.github.com>
2020-05-14 17:53:08 +0200

    Agginta logica corsie e differenziazione strada

--- Progetto/cube.py
+++ Progetto/cube.py
@@ -13,9 +13,11 @@ class cube():
     def recognition(self, pos_x, pos_y):
         for j in points:
             if(j[0] == pos_x and j[1] == pos_y):
-                self.name = j[2] 
+                self.name = j[2]
                 self.pos = j[3]
-                self.sens = j[4]
+                self.cors = j[4]
+                self.sens = j[5]
+                break;
         cube = None
         if(self.val == 0):
             cube = arcade.load_texture("../Concept Art/Blocks/bound.png")
@@ -27,13 +29,58 @@ class cube():
             cube = arcade.load_texture("../Concept Art/Blocks/end.png")
             cube.draw_scaled(self.center_x, self.center_y)
         if(self.val == 3):
-            cube = arcade.load_texture("../Concept Art/Blocks/road.png")
-            cube.draw_scaled(self.center_x, self.center_y)
+            if(self.sens == "in"):
+                if(self.cors == "left"):
+                    cube = arcade.load_texture("../Concept Art/Blocks/road1.png")
+                    cube.draw_scaled(self.center_x, self.center_y)
+                if(self.cors == "right"):
+                    cube = arcade.load_texture("../Concept Art/Blocks/road2.png")
+                    cube.draw_scaled(self.center_x, self.center_y)
+            if(self.sens == "out"):
+                if(self.cors == "left"):
+                    cube = arcade.load_texture("../Concept Art/Blocks/road3.png")
+                    cube.draw_scaled(self.center_x, self.center_y)
+                if(self.cors == "right"):
+                    cube = arcade.load_texture("../Concept Art/Blocks/road4.png")
+                    cube.draw_scaled(self.center_x, self.center_y)
+            
         if(self.val == 4):
             cube = arcade.load_texture("../Concept Art/Blocks/stop.png")
             cube.draw_scaled(self.center_x, self.center_y)
+        if(self.val == 5):
+            cube = arcade.load_texture("../Concept Art/Blocks/road.png")
+            cube.draw_scaled(self.center_x, self.center_y)
         self.cube = cube
     
-#    def next_step(self, pos_x, pos_y):
+#    def next_step(self):
 #        for j in points:
-#            if(j[0] == pos_x and j[1] == pos_y):
+#            if(j[0] == self.pos_x and j[1] == self.pos_y):
+#                if(self.pos == "nord"):
+#                    if(self.sens == "in"):
+#                        if(self.cors == "left"):
+#                        if(self.cors == "right"):
+#                    if(self.sens == "out"):
+#                        if(self.cors == "left"):
+#                        if(self.cors == "right"):
+#                if(self.pos == "sud"):
+#                    if(self.sens == "in"):
+#                        if(self.cors == "left"):
+#                        if(self.cors == "right"):
+#                    if(self.sens == "out"):
+#                        if(self.cors == "left"):
+#                        if(self.cors == "right"):
+#                if(self.pos == "est"):
+#                    if(self.sens == "in"):
+#                        if(self.cors == "left"):
+#                        if(self.cors == "right"):
+#                    if(self.sens == "out"):
+#                        if(self.cors == "left"):
+#                        if(self.cors == "right"):
+#                if(self.pos == "ovest"):
+#                    if(self.sens == "in"):
+#                        if(self.cors == "left"):
+#                        if(self.cors == "right"):
+#                    if(self.sens == "out"):
+#                        if(self.cors == "left"):
+#                        if(self.cors == "right"):
+
